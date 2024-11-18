8440264151968ab838517a158c943ec4d9c09988
Atikur Rahman Rajib <114246760+atikurajib@users.noreply.github.com>
2024-11-19 01:26:18 +0600

    Add files via upload

--- Java/hello_world.java
+++ Java/hello_world.java
@@ -1,7 +1,8 @@
-// Printing Hello World
+// Print Hello World in Java.
 
 public class Main {
-	public static void main(String[] args) {
-  	System.out.println("Hello World"); //print Hello World
-  }
+    public static void main(String[] args) {
+        // Prints the string "Hello World" to the console
+        System.out.println("Hello World");
+    }
 }
