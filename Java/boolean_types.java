8ad946c8be3046818204a529b1fd01e2a4039994
Atikur Rahman Rajib <114246760+atikurajib@users.noreply.github.com>
2024-11-19 01:40:42 +0600

    Add files via upload

---
+++ Java/boolean_types.java
@@ -0,0 +1,13 @@
+// Demonstrating Boolean types in Java.
+
+public class Main {
+    public static void main(String[] args) {
+        // Boolean variables: stores true or false values
+        boolean isJavaFun = true;       // True value indicating that Java is fun
+        boolean isFuskaTasty = false;   // False value indicating that Fuska is not tasty (just an example)
+
+        // Print the values of the Boolean variables
+        System.out.println(isJavaFun);   // Output: true
+        System.out.println(isFuskaTasty); // Output: false
+    }
+}
