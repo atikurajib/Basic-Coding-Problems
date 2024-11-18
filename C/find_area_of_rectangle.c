247e55f49a2f6263cbb5e5592f0cc98217485693
Atikur Rahman Rajib <114246760+atikurajib@users.noreply.github.com>
2024-11-19 00:49:19 +0600

    Add files via upload

--- C/find_area_of_rectangle.c
+++ C/find_area_of_rectangle.c
@@ -1,16 +1,24 @@
 //The length & breadth of a rectangle are input through the keyboard. Find out the rectangle’s area
 
-#include<stdio.h>
+#include<stdio.h> // Include standard input/output library
+
 int main()
 {
-    int length, breadth, area; // Declare variables
-    // Get user input for length and breadth of the rectangle
-    printf("Enter the length of rectangle: ");
+    int length, breadth, area; // Declare variables for length, breadth, and area
+
+    // Prompt the user to input the length of the rectangle
+    printf("Enter the length of the rectangle: ");
     scanf("%d", &length);
-    printf("Enter the breadth of rectangle: ");
-    scanf("%d", &breadth);    
-    // Calculate and display the area of the rectangle
+
+    // Prompt the user to input the breadth of the rectangle
+    printf("Enter the breadth of the rectangle: ");
+    scanf("%d", &breadth);
+
+    // Calculate the area of the rectangle
     area = length * breadth;
-    printf("\nArea of Rectangle is %d\n", area);    
-    return 0; // Successful program execution
+
+    // Display the calculated area
+    printf("\nArea of the Rectangle is %d\n", area);
+
+    return 0; // Indicate successful program execution
 }
