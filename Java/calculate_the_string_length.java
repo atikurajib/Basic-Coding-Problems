f8673c19eb5b8595c1d320fc8f25db1efddcd17d
Atikur Rahman Rajib <114246760+atikurajib@users.noreply.github.com>
2024-11-19 02:00:56 +0600

    Add files via upload

---
+++ Java/calculate_the_string_length.java
@@ -0,0 +1,9 @@
+// Calculate the length of a string in Java.
+
+public class Main {
+    public static void main(String[] args) {
+        String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // Define a string
+        // Use the length() method to find the number of characters in the string
+        System.out.println("The length of the txt string is: " + txt.length());  // Prints the length (26)
+    }
+}
