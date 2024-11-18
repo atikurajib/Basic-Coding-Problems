deef906134685983a9f34880a1538ba2388a8e61
Atikur Rahman Rajib <114246760+atikurajib@users.noreply.github.com>
2024-11-19 00:50:08 +0600

    Add files via upload

--- C/hello_world.c
+++ C/hello_world.c
@@ -1,8 +1,9 @@
 //Printing Hello World!
 
-#include<stdio.h>
-int main ()
+#include<stdio.h> // Include standard input/output library
+
+int main()
 {
-    printf("Hello World!\n"); //print Hello World!
-    return 0;
+    printf("Hello World!\n"); // Print "Hello World!" with a newline
+    return 0; // Indicate successful program execution
 }
