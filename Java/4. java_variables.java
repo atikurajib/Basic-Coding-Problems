8d26c549aa1b5db50b3c80923b6a5bd3db3d781b
Atikur Rahman Rajib <114246760+atikurajib@users.noreply.github.com>
2024-11-19 01:28:21 +0600

    Add files via upload

---
+++ Java/4. java_variables.java
@@ -0,0 +1,29 @@
+// Demonstrating the use of various Java variable types.
+
+public class Main {
+    public static void main(String[] args) {
+        // String variable: stores a sequence of characters
+        String name = "Ahmed";
+        System.out.println(name);
+        
+        // Integer variable: stores a whole number
+        int number1 = 11;
+        System.out.println(number1);
+        
+        // Float variable: stores a floating-point number (less precision than double)
+        float number2 = 4.25f;
+        System.out.println(number2);
+        
+        // Double variable: stores a floating-point number (higher precision)
+        double number3 = 3.33;
+        System.out.println(number3);
+        
+        // Char variable: stores a single character
+        char ch = 'A';
+        System.out.println(ch);
+        
+        // Boolean variable: stores true or false value
+        boolean decision = true;
+        System.out.println(decision);
+    }
+}
