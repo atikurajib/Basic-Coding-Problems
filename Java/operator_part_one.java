2a8a629e60d71865322acca54cce929dc2dc9df4
Atikur Rahman Rajib <114246760+atikurajib@users.noreply.github.com>
2024-11-19 01:50:56 +0600

    Add files via upload

---
+++ Java/operator_part_one.java
@@ -0,0 +1,9 @@
+// Increment Operator: Increases the value of the variable by 1
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 5;     // Initialize variable x with value 5
+        ++x;            // Increment operator: x is increased by 1 (x becomes 6)
+        System.out.println(x);  // Prints the incremented value (6)
+    }
+}
