3f8ac034e28d0e6a302704fa3dba17c2afabff9b
Atikur Rahman Rajib <114246760+atikurajib@users.noreply.github.com>
2024-11-19 02:04:51 +0600

    Add files via upload

---
+++ Java/find_a_string_position.java
@@ -0,0 +1,10 @@
+// Find the position (index) of a substring within a string.
+
+public class Main {
+    public static void main(String[] args) {
+        String txt = "My name is Rajib";  // Define a string
+        
+        // Find the position of the substring "Rajib" in the string
+        System.out.println(txt.indexOf("Rajib"));  // Output: 11
+    }
+}
