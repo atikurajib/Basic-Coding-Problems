3519fe92991e918eb81f93c74d529a0e9e5eb6c3
Atikur Rahman Rajib <114246760+atikurajib@users.noreply.github.com>
2024-11-19 02:02:01 +0600

    Add files via upload

---
+++ Java/convert_uppercase_and_lowercase.java
@@ -0,0 +1,13 @@
+// Convert string to uppercase and lowercase in Java.
+
+public class Main {
+    public static void main(String[] args) {
+        String txt = "Atikur Rahman Rajib";  // Define a string
+
+        // Convert the string to uppercase and print the result
+        System.out.println(txt.toUpperCase());  // Output: "ATIKUR RAHMAN RAJIB"
+
+        // Convert the string to lowercase and print the result
+        System.out.println(txt.toLowerCase());  // Output: "atikur rahman rajib"
+    }
+}
