27dbb4d549da165a3144d121215f9211e213407b
Atikur Rahman Rajib <114246760+atikurajib@users.noreply.github.com>
2024-11-19 01:51:43 +0600

    Add files via upload

---
+++ Java/operator_part_two.java
@@ -0,0 +1,9 @@
+// Decrement Operator: Decreases the value of the variable by 1
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 5;     // Initialize variable x with value 5
+        --x;            // Decrement operator: x is decreased by 1 (x becomes 4)
+        System.out.println(x);  // Prints the decremented value (4)
+    }
+}
