069f2ff11921e57a967e814aa66524504b3dad7a
Atikur Rahman Rajib <114246760+atikurajib@users.noreply.github.com>
2024-11-19 01:53:38 +0600

    Add files via upload

---
+++ Java/type_casting_part_one.java
@@ -0,0 +1,10 @@
+// Widening Casting (Implicit Casting): Converting a smaller type to a larger type automatically.
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 11;             // Integer value
+        double y = x;           // Widening casting: int to double (automatic)
+        System.out.println("Integer x: " + x);  // Prints integer value
+        System.out.println("Double y: " + y);   // Prints double value (automatic casting result)
+    }
+}
