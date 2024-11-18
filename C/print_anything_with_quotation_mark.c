2e2c2847d8bceea3c4326d0c3acdec09bbd373d7
Atikur Rahman Rajib <114246760+atikurajib@users.noreply.github.com>
2024-11-19 00:58:29 +0600

    Add files via upload

---
+++ C/print_anything_with_quotation_mark.c
@@ -0,0 +1,11 @@
+// Print a line in quotation marks.
+
+#include<stdio.h> // Include standard input/output library
+
+int main()
+{
+    // Print "Hello World" with quotation marks
+    printf("\"Hello World\"\n");
+
+    return 0; // Indicate successful program execution
+}
