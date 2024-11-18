79ec1338b6ab72e5f0f87616e80bb2adbe009cb1
Atikur Rahman Rajib <114246760+atikurajib@users.noreply.github.com>
2024-11-19 01:39:26 +0600

    Add files via upload

---
+++ Java/calculate_the_area_of_rectangle.java
@@ -0,0 +1,18 @@
+// Calculate the Area of a Rectangle.
+
+public class Main {
+    public static void main(String[] args) {
+        // Initialize the length and width of the rectangle
+        int length = 10;    // Length of the rectangle
+        int width = 15;     // Width of the rectangle
+        int area;           // Variable to store the area of the rectangle
+        
+        // Calculate the area of the rectangle
+        area = length * width;  // Area formula: length * width
+        
+        // Print the values of length, width, and the calculated area
+        System.out.println("Length: " + length);
+        System.out.println("Width: " + width);
+        System.out.println("Area of Rectangle: " + area);
+    }
+}
