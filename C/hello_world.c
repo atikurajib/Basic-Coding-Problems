b3a26a84d3900804fc16c4b2be8454854d92c65b
Atikur Rahman Rajib <114246760+atikurajib@users.noreply.github.com>
2025-01-08 19:35:22 +0600

    Delete C directory

--- C/hello_world.c
+++
@@ -1,9 +0,0 @@
-// Printing Hello World!
-
-#include<stdio.h> // Include standard input/output library
-
-int main()
-{
-    printf("Hello World!\n"); // Print "Hello World!" with a newline
-    return 0; // Indicate successful program execution
-}
